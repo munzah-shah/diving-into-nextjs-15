bcf3e021a732a02a7a44d0611908e9e492bdd45f
Munzah Shah <munzah.shah00@gmail.com>
2025-04-10 08:23:28 +0500

    loading file

---
+++ nextjs-15-blog/src/app/loading.jsx
@@ -0,0 +1,9 @@
+import Loader from "../components/Loader";
+
+export default function Loading() {
+  return (
+    <div className="h-[90vh] w-full flex items-center justify-center">
+      <Loader />
+    </div>
+  );
+}
