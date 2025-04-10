b84322b5c8beafae63ce9b571974747df1c49113
Munzah Shah <munzah.shah00@gmail.com>
2025-04-10 08:22:57 +0500

    home page

--- nextjs-15-blog/src/app/page.jsx
+++ nextjs-15-blog/src/app/page.jsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <h1>Home Page</h1>;
+import { getCollection } from "../lib/db";
+import PostCard from "../components/PostCard";
+
+export default async function Home() {
+  const postsCollection = await getCollection("posts");
+  const posts = await postsCollection?.find().sort({ $natural: -1 }).toArray();
+
+  if (posts) {
+    return (
+      <div className="grid grid-cols-2 gap-6">
+        {posts.map((post) => (
+          <div key={post._id}>
+            <PostCard post={post} />
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return <p>Failed to fetch data from the database.</p>;
+  }
 }
